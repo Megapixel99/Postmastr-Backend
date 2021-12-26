e1431a81af030ee40f8ce2de280a1b140e203af9
Joseph Tang <josephtang724@gmail.com>
2021-12-25 21:57:05 -0800

    testing

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -4,7 +4,7 @@ const User = require('/Users/josephtang/seniorCapstone/MVP/server/models/User.js
 app.post('*', (req, res) => {
     connectDB()
         .then(() => {
-            return User.create(req.body);
+            return 2;
         })
         .then(userItem => {
             res.status(200).json({
