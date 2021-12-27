ffd2288ea267b58c49b3236782c79b18c2df8824
Joseph Tang <josephtang724@gmail.com>
2021-12-27 12:25:05 -0800

    fixed user endpoint method

--- api/user/index.js
+++ api/user/index.js
@@ -1,10 +1,12 @@
-const app = require('../../util/configureApi');
-const connectDB = require('/Users/josephtang/seniorCapstone/MVP/util/db.js');
-const User = require('/Users/josephtang/seniorCapstone/MVP/models/User.js');
+const app = require('../../util/configureApi.js');
+const connectDB = require('../../util/db.js');
+const User = require('../../models/User.js');
 app.get('*', (req, res) => {
     connectDB()
         .then(() => {
-            newUser = new User(req.body);
+
+            res.status(200).send('endpoint reached for inserting user');
+            res.end();
 
         })
         .catch(error => {
