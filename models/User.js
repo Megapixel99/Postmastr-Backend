b1f4a564872dda999ea48265c5b27b903c99c91f
Joseph Tang <josephtang724@gmail.com>
2022-01-12 14:04:43 -0800

    fixed brcypt path in user model file

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, default: "Jim" },
     password: { type: String, required: true, default: "pass" }
