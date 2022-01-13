66b76141525fbf0b5f5df5aa7a0aca01f37a9fb4
Joseph Tang <josephtang724@gmail.com>
2022-01-12 17:52:36 -0800

    modified recipient Schema

--- models/Recipient.js
+++ models/Recipient.js
@@ -3,6 +3,6 @@ const recipientSchema = new mongoose.Schema({
     name: { type: String, required: true },
     boxNumber: { type: Number, required: true },
     email: { type: String, required: true },
-    idNumber: { type: Number, required: true }
+    idNumber: { type: String, required: true }
 });
 module.exports = mongoose.model("recipient", recipientSchema);
