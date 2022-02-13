96c8031aeca154d37247c1f2b92f85ba977a2914
Seth Wheeler (Student) <wheelersc@cox.net>
2022-02-12 16:13:19 -0800

    implement sessions

--- routers/client.js
+++ routers/client.js
@@ -10,36 +10,55 @@ function prepareSource(filePath, data, templateName = 'main') {
   return template(data);
 }
 
-router.get('/', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/dashboard.hbs`, {}));
-});
-
-router.get('/susPackageReport', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/susPackageReport.hbs`, {}));
-});
-
 router.get('/login', (req, res) => {
   res.sendFile(path.resolve('./assets/html/login.html'));
-});
-
-router.get('/usageStatistics', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/usageStatistics.hbs`, {}));
-});
-
-router.get('/maps', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/maps.hbs`, {}));
-});
-
-router.get('/recipientInfo', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/recipientInfo.hbs`, {}));
 });
 
 router.get('/register', (req, res) => {
   res.sendFile(path.resolve('./assets/html/register.html'));
 });
 
+router.use((req, res, next) => {
+  if (!req.session.username) {
+    return res.redirect(301, '/login');
+  }
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.send(prepareSource(`${__dirname}/../assets/hbs/dashboard.hbs`, {
+    username: req.session.username,
+  }));
+});
+
+router.get('/susPackageReport', (req, res) => {
+  res.send(prepareSource(`${__dirname}/../assets/hbs/susPackageReport.hbs`, {
+    username: req.session.username,
+  }));
+});
+
+router.get('/usageStatistics', (req, res) => {
+  res.send(prepareSource(`${__dirname}/../assets/hbs/usageStatistics.hbs`, {
+    username: req.session.username,
+  }));
+});
+
+router.get('/maps', (req, res) => {
+  res.send(prepareSource(`${__dirname}/../assets/hbs/maps.hbs`, {
+    username: req.session.username,
+  }));
+});
+
+router.get('/recipientInfo', (req, res) => {
+  res.send(prepareSource(`${__dirname}/../assets/hbs/recipientInfo.hbs`, {
+    username: req.session.username,
+  }));
+});
+
 router.get('/packageInfo', (req, res) => {
-  res.send(prepareSource(`${__dirname}/../assets/hbs/packageInfo.hbs`, {}));
+  res.send(prepareSource(`${__dirname}/../assets/hbs/packageInfo.hbs`, {
+    username: req.session.username,
+  }));
 });
 
 module.exports = router;
