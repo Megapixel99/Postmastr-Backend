931aea6bd54b75054a06c07c13d3ce62ed33dbd6
Joseph Tang <josephtang724@gmail.com>
2022-02-12 21:32:42 -0800

    reformatting image processing structure

--- routers/api/imageProcessing/index.js
+++ routers/api/imageProcessing/index.js
@@ -1,19 +1,14 @@
 const app = require('express').Router();
-
-const multer = require('multer')
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const { multer, tesseract } = require('../../../util');
 
 
+app.post('*', upload.single(scan), (req, res) => {
+    res.sendstatus(202)
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, path.join(__dirname, './images'));
-    },
-    filename(req, file, cb) {
-        cb(null, `${uuidv4()}.${file.mimetype.split('/')[1]}`);
-    },
+
 });
 
-module.exports = { app, multer({ storage }) };
 
+
+
+module.exports = app;
--- util/index.js
+++ util/index.js
@@ -1,3 +1,8 @@
 exports.environment = require('./environment.js');
 exports.sessions = require('./sessions.js');
 exports.db = require('./db.js');
+<<<<<<< Updated upstream
+=======
+exports.multer = require('./multer.js');
+exports.tesseract = require('./tesseract.js');
+>>>>>>> Stashed changes
