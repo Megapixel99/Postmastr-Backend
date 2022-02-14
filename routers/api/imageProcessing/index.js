638a2cfa3b86b118fc2fe5cc8c3d4916daa37d84
Joseph Tang <josephtang724@gmail.com>
2022-02-14 15:52:09 -0800

    Successful implementation of Tesseract

--- routers/api/imageProcessing/index.js
+++ routers/api/imageProcessing/index.js
@@ -1,7 +1,7 @@
 const app = require('express').Router();
 const multer = require('multer');
 const { tesseract } = require('../../../util');
-//const { storage } = require('../../../util');
+
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,13 +15,12 @@ var storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
-app.post('*', upload.single('image'), (req, res) => {
+app.post('*', upload.single('image'), async (req, res) => {
 
     //begin reg struct
     const img = req.file.path;
-    //console.log(img);
 
-    const labelData = tesseract(img);
+    const labelData = (await tesseract(img));
     console.log(labelData);
     return res.status(200).json({
         result: {
@@ -33,19 +32,3 @@ app.post('*', upload.single('image'), (req, res) => {
 
 });
 module.exports = app;
-
-/*const router = require('express').Router();
-const multer = require('multer');
-
-
-
-const upload = multer({ storage: storage });
-
-
-router.post('*', upload.single('image'), (req, res) => {
-    res.sendStatus(202);
-    console.log("In route:");
-    console.log(req.file);
-});
-
-module.exports = router; */
