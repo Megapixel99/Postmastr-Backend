3337b9293596b16dedc4a9148cc4561619e5791d
Joseph Tang <josephtang724@gmail.com>
2022-02-26 23:46:47 -0800

    Implent optimization measures for scan route

--- routers/api/imageProcessing/index.js
+++ routers/api/imageProcessing/index.js
@@ -4,9 +4,6 @@ const { tesseract, } = require('../../../util');
 const sharp = require('sharp');
 const auth = require("../../../middleware/auth");
 
-
-
-
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         console.log("setting dest");
@@ -22,30 +19,27 @@ const upload = multer({ storage: storage });
 app.post('*',/* auth,*/ upload.single('image'), async (req, res) => {
 
     //begin reg struct
-    const img = req.file.path;
+    img = req.file.path;
     const metadata = await sharp(req.file.path).metadata();
     console.log("Acquire Original Dimensions");
     const origWidth = metadata.width;
     const origHeight = metadata.height;
     console.log("Original dimensions acquired");
     console.log(req.file.path);
-
-
-    console.log("begin resizing");
-    const resizedImage = /*Buffer.from(
-        await sharp(req.file.path).resize(1200, 1350,
-            { fit: 'contain' }).toBuffer(), 'hex');
-*/
-        await sharp(req.file.path).resize(1000, 1000, {
-            fit: 'contain'
-        }).toFile('processed.png')
-            .then(newImg => {
-                console.log("resizing successful");
-                return newImg;
-            });
-    console.log(resizedImage);
-    //const ocrInput = await toBase64(resizedImage);
-    const labelData = (await tesseract('processed.png'));
+    if ((origWidth < 1000) && (origHeight < 1000)) {
+        console.log("begin resizing");
+        const resizedImage =
+            await sharp(req.file.path).resize(1000, 1000, {
+                fit: 'contain'
+            }).toFile('processed.png')
+                .then(newImg => {
+                    console.log("resizing successful");
+                    return newImg;
+                });
+        img = 'processed.png';
+        console.log(resizedImage);
+    }
+    const labelData = (await tesseract(img));
     console.log(labelData);
 
     return res.status(200).json({
