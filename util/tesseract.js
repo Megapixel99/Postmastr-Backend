8fd2cc90cef6eb64ee2fadbc3af2bc8ddce435df
Brandon Watanabe <brandonw0007@gmail.com>
2022-03-13 14:53:34 -0700

    Update tesseract.js
    implemented regex to locate box number on mail label

--- util/tesseract.js
+++ util/tesseract.js
@@ -1,6 +1,7 @@
 const { createWorker } = require('tesseract.js');
 const path = require('path');
 const app = require('express').Router();
+const models = require('../models/models.js');
 
 module.exports = function (imagePath) {
     return (async () => {
@@ -13,7 +14,32 @@ module.exports = function (imagePath) {
         await worker.initialize('eng');
         const { data: { text } } = await worker.recognize(imagePath);
         await worker.terminate();
-        capsText = text.toUpperCase();//Convert text to uppercase for uniformity
+        capsText = text.toUpperCase(); //Convert text to uppercase for uniformity
+
+        const regex = RegExp(/#\s[0-9]{4}/);
+        let boxNum = parseInt(regex.exec(text).toString().substring(2));
+        console.log(boxNum);
+        const matches = [];
+
+        models.Recipient.find( { boxNumber: boxNum }, function(err, recipient) {
+            console.log("test");
+            if (err) {
+                console.error('Unexpected error occured:');
+                console.error(err);
+                return;
+            } if (!recipient || recipient.length === 0) {
+                console.log('No box number was found');
+                return;
+            }
+            for (let i = 0; i < recipient.length; i += 1) {
+                if (boxNum === recipient[i].boxNumber) {
+                    console.log("it worked!!!!!!!!!!!!!!!!!");
+                    matches.push(recipient[i]);
+                    console.log(matches);
+                }
+            }
+        });
+
         //split label at Ship To: or To: line 
         if (capsText.includes("SHIP TO/:/g") || capsText.includes("SHIP\nTO/:/g")) {
             sliceStart = capsText.indexOf("SHIP");
