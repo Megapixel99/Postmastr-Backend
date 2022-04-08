2156b8abe78644f0c49574b8bb37d1766717efc3
Joseph Tang <josephtang724@gmail.com>
2022-04-07 18:36:11 -0700

    Initial implementation of email feature, fix typo

--- routers/api/package/input/index.js
+++ routers/api/package/input/index.js
@@ -1,7 +1,8 @@
 const app = require('express').Router();
 const connectDB = require('../../../../util/db.js');
 const { Package } = require('../../../../models/models.js');
-const nodemailer = require("nodemailer");
+const {nodeMailer} = require('../../../../util');
+
 app.post('*', (req, res) => {
     connectDB()
         .then(() => {
@@ -15,38 +16,24 @@ app.post('*', (req, res) => {
 
             } else {
                 const recipient = req.body.recipient;
- 
+
                 const carrier = req.body.carrierName;
-              
+
                 const trackingNo = req.body.trackingNumber;
+                const email = "jjtang18@apu.edu";
                 const newPackage = new Package({
                     recipient: recipient,
                     carrierName: carrier,
-                    recipientAddress: toAddress,
                     trackingNumber: trackingNo,
                     dateRecieved: new Date(),
                 })
-                const transporter = nodemailer.createTransport({
-                    host: 'smtp.ethereal.email',
-                    port: 587,
-                    auth: {
-                        user: 'evelyn.vandervort92@ethereal.email',
-                        pass: 'pZCd8GrvNeCxr3a4tY'
-                    }
-                });
-                let info = await transporter.sendMail({
-                    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-                    to: "bar@example.com, baz@example.com", // list of receivers
-                    subject: "Hello ✔", // Subject line
-                    text: "Hello world?", // plain text body
-                    html: "<b>Hello world?</b>", // html body
-                  });
-
                 newPackage.save().then(() => {
+                    nodeMailer(newPackage);
+                }).then(()=>{
                     return res.status(201).json({
-                        result: newPackage
-                    })
-
+                      package: newPackage,
+                      message: "package logged"
+                    });
                 })
 
 
