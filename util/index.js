853994504c9f35ddedc022e95e832daa6802b8e1
Joseph Tang <josephtang724@gmail.com>
2022-04-07 18:36:11 -0700

    Initial implementation of email feature, fix typo

--- util/index.js
+++ util/index.js
@@ -2,5 +2,6 @@ exports.environment = require('./environment.js');
 exports.sessions = require('./sessions.js');
 exports.db = require('./db.js');
 exports.tesseract = require('./tesseract.js');
+exports.nodeMailer = require('./nodeMailer.js');
 
 
