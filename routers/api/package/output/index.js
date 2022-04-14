117b732928f2d49686752da89384e40914d5d44f
Brandon Watanabe <brandonw0007@gmail.com>
2022-04-14 15:07:39 -0700

    Update index.js
    added route for packageCheckout button

--- routers/api/package/output/index.js
+++ routers/api/package/output/index.js
@@ -2,9 +2,9 @@ const app = require('express').Router();
 const connectDB = require('../../../../util/db.js');
 const { Package } = require('../../../../models/models.js');
 
-app.get('*', (req, res) => {
+app.get('*/:uuid', (req, res) => {
   connectDB()
-  .then(() => Package.find(req.query))
+  .then(() => Package.findOne({ uuid: req.params.uuid }))
   .then(packages => res.status(200).json(packages))
   .catch(error =>
     res.status(error.statusCode || 500)
@@ -12,9 +12,22 @@ app.get('*', (req, res) => {
   );
 });
 
-app.get('*/:uuid', (req, res) => {
+app.get('/:packageID', (req, res) => {
   connectDB()
-  .then(() => Package.findOne({ uuid: req.params.uuid }))
+  .then(() => Package.findOneAndUpdate({id: req.params.packageID}
+  , {pickedUp: true}))
+  .then(function (package) {
+    res.send(package)
+  })
+  .catch(error =>
+    res.status(error.statusCode || 500)
+    .json({ error: error.message })
+  );
+});
+
+app.get('*', (req, res) => {
+  connectDB()
+  .then(() => Package.find(req.query))
   .then(packages => res.status(200).json(packages))
   .catch(error =>
     res.status(error.statusCode || 500)
